8e13d8d2a62d074f1214a95fb15d14ed827510d6
0ankit0 <ankitpaudel.1373@gmail.com>
2026-01-07 06:57:22 +0545

    Enhance Django REST framework setup and documentation
    - Updated Pipfile to include additional packages: djangorestframework-simplejwt for JWT authentication, django-filter for advanced filtering, and drf-spectacular for OpenAPI schema generation.
    - Modified Pipfile.lock to reflect new package versions and hashes.
    - Fixed import in urls.py to correctly include the URLs from the Django REST framework.
    - Improved documentation in the Jupyter notebook to clarify filtering, searching, and ordering capabilities in DRF, providing examples for better understanding.

--- config/urls.py
+++ config/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-from rest_framework import routers
+from rest_framework import routers,urls
 from apps.user_management import views
 
 router = routers.DefaultRouter()
@@ -26,5 +26,5 @@ router.register(r'groups', views.GroupViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include(router.urls)),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include(urls, namespace='rest_framework')),
 ]
