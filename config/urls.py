e6d38b3fed18869ff3d189aa8079352c00be5415
0ankit0 <ankitpaudel.1373@gmail.com>
2026-07-18 11:36:55 +0545

    feat: added token create and refresh endpoints using simplejwt

--- config/urls.py
+++ config/urls.py
@@ -15,12 +15,21 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import include, path
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework_simplejwt.views import (
+    TokenBlacklistView,
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/token/blacklist/', TokenBlacklistView.as_view(), name='token_blacklist'),
+
 ]
 if settings.DEBUG:
     urlpatterns += static(
