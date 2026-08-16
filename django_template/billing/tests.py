a3421d5a8d242f66d842d2d7523e6a3b57aab340
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-17 04:47:58 +0545

    test(billing): fix billing tests

--- django_template/billing/tests.py
+++ django_template/billing/tests.py
@@ -6,10 +6,8 @@ import time
 import pytest
 from django.test import override_settings
 
-from .models import Feature
 from .models import Price
 from .models import Product
-from .services import has_feature
 from .services import verify_webhook_signature
 
 
@@ -20,16 +18,6 @@ def test_price_amount_is_smallest_currency_unit():
 
     assert price.amount_decimal == 29.99
     assert str(price) == "Pro - 29.99 USD / month"
-
-
-@pytest.mark.django_db
-def test_feature_is_available_only_when_subscription_is_active(tenant):
-    product = Product.objects.create(name="Pro", slug="pro")
-    price = Price.objects.create(product=product, amount=2900)
-    feature = Feature.objects.create(key="api", name="API")
-    product.product_features.create(feature=feature)
-
-    assert has_feature(tenant, "api") is False
 
 
 def test_stripe_signature_verification():
