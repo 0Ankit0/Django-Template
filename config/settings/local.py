58a734bb608b752696f179ad0af5469afb66e0f6
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-17 04:47:19 +0545

    feat(billing): enable billing and stripe settings in local environment

--- config/settings/local.py
+++ config/settings/local.py
@@ -7,19 +7,10 @@ from .base import MIDDLEWARE
 from .base import env
 
 # GENERAL
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#debug
 DEBUG = True
-# https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
-SECRET_KEY = env(
-    "DJANGO_SECRET_KEY"
-)
-# https://docs.djangoproject.com/en/dev/ref/settings/#allowed-hosts
+SECRET_KEY = env("DJANGO_SECRET_KEY")
 ALLOWED_HOSTS = ["localhost", ".localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S104
 
-# CACHES
-# ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
@@ -27,40 +18,23 @@ CACHES = {
     },
 }
 
-# EMAIL
-# ------------------------------------------------------------------------------
 EMAIL_BACKEND = env.str(
-    "DJANGO_EMAIL_BACKEND", 
-    default="django.core.mail.backends.console.EmailBackend"
+    "DJANGO_EMAIL_BACKEND",
+    default="django.core.mail.backends.console.EmailBackend",
 )
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
 EMAIL_HOST = env("EMAIL_HOST")
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = 1025
 
-# WhiteNoise
-# ------------------------------------------------------------------------------
-# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
 INSTALLED_APPS = ["whitenoise.runserver_nostatic", *INSTALLED_APPS]
-
-
-# django-debug-toolbar
-# ------------------------------------------------------------------------------
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ["debug_toolbar"]
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": [
         "debug_toolbar.panels.redirects.RedirectsPanel",
-        # Disable profiling panel due to an issue with Python 3.12+:
-        # https://github.com/jazzband/django-debug-toolbar/issues/1875
         "debug_toolbar.panels.profiling.ProfilingPanel",
     ],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER") == "yes":
     import socket
@@ -68,13 +42,15 @@ if env("USE_DOCKER") == "yes":
     hostname, _, ips = socket.gethostbyname_ex(socket.gethostname())
     INTERNAL_IPS += [".".join([*ip.split(".")[:-1], "1"]) for ip in ips]
 
-# django-extensions
-# ------------------------------------------------------------------------------
-# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]
 
-# Django Control Room
-# ------------------------------------------------------------------------------
+# Billing is shared/public-schema data because subscriptions belong to tenants.
+INSTALLED_APPS += ["django_template.billing"]
+
+STRIPE_SECRET_KEY = env.str("STRIPE_SECRET_KEY", default="")
+STRIPE_PUBLISHABLE_KEY = env.str("STRIPE_PUBLISHABLE_KEY", default="")
+STRIPE_WEBHOOK_SECRET = env.str("STRIPE_WEBHOOK_SECRET", default="")
+
 DJ_CONTROL_ROOM_SETTINGS = {
     **DJ_CONTROL_ROOM_SETTINGS,
     "REGISTER_PANELS_IN_ADMIN": env.bool("CR_REGISTER_PANELS", default=True),
@@ -87,38 +63,34 @@ DJ_CONTROL_ROOM_SETTINGS = {
         "controlroom_sentry": env.bool("CR_REGISTER_SENTRY_PANEL", default=True),
     },
 }
-REDIS_URL = env.str("REDIS_URL", default="redis://redis:6379/0") 
-redis_url = urlparse(url=REDIS_URL) 
+REDIS_URL = env.str("REDIS_URL", default="redis://redis:6379/0")
+redis_url = urlparse(url=REDIS_URL)
 DJ_REDIS_PANEL_SETTINGS = {
-     "ALLOW_KEY_DELETE": False, 
-     "ALLOW_KEY_EDIT": False, 
-     "ALLOW_TTL_UPDATE": False, 
-     "CURSOR_PAGINATED_SCAN": False, 
-     "CURSOR_PAGINATED_COLLECTIONS": False, 
-     "socket_timeout": 5.0, 
-     "socket_connect_timeout": 5.0, 
-     "INSTANCES": { 
-         "local_redis": { 
-             "description": "Local Redis Instance", 
-             "host": redis_url.hostname or "redis", 
-             "port": redis_url.port or 6379, 
-             "features": { 
-                 "ALLOW_KEY_DELETE": True, 
-                 "ALLOW_KEY_EDIT": True, 
-                 "ALLOW_TTL_UPDATE": True, 
-                 "CURSOR_PAGINATED_SCAN": True, 
-                 "CURSOR_PAGINATED_COLLECTIONS": True, 
-            }, 
-        }, 
-    }, 
+    "ALLOW_KEY_DELETE": False,
+    "ALLOW_KEY_EDIT": False,
+    "ALLOW_TTL_UPDATE": False,
+    "CURSOR_PAGINATED_SCAN": False,
+    "CURSOR_PAGINATED_COLLECTIONS": False,
+    "socket_timeout": 5.0,
+    "socket_connect_timeout": 5.0,
+    "INSTANCES": {
+        "local_redis": {
+            "description": "Local Redis Instance",
+            "host": redis_url.hostname or "redis",
+            "port": redis_url.port or 6379,
+            "features": {
+                "ALLOW_KEY_DELETE": True,
+                "ALLOW_KEY_EDIT": True,
+                "ALLOW_TTL_UPDATE": True,
+                "CURSOR_PAGINATED_SCAN": True,
+                "CURSOR_PAGINATED_COLLECTIONS": True,
+            },
+        },
+    },
 }
-# Celery
-# ------------------------------------------------------------------------------
 
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
 CELERY_TASK_EAGER_PROPAGATES = True
 
-# Adding local file storage for development
 STORAGES = {
     "default": {
         "BACKEND": "django.core.files.storage.FileSystemStorage",
