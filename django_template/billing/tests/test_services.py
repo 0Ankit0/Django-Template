e315a96d5eaff1bad246549ac0f9edc613f05be8
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-25 12:41:58 +0545

    test: cover expiring entitlement interval calculation

---
+++ django_template/billing/tests/test_services.py
@@ -0,0 +1,24 @@
+from datetime import datetime
+
+from django.test import SimpleTestCase
+
+from django_template.billing.models import Price
+from django_template.billing.services import _add_interval
+
+
+class EntitlementIntervalTests(SimpleTestCase):
+    def test_days(self):
+        start = datetime(2026, 1, 1, 12, 0)
+        self.assertEqual(_add_interval(start, Price.Interval.DAY, 7), datetime(2026, 1, 8, 12, 0))
+
+    def test_weeks(self):
+        start = datetime(2026, 1, 1, 12, 0)
+        self.assertEqual(_add_interval(start, Price.Interval.WEEK, 2), datetime(2026, 1, 15, 12, 0))
+
+    def test_month_clamps_end_of_month(self):
+        start = datetime(2026, 1, 31, 12, 0)
+        self.assertEqual(_add_interval(start, Price.Interval.MONTH, 1), datetime(2026, 2, 28, 12, 0))
+
+    def test_year_preserves_calendar_month_and_clamps_day(self):
+        start = datetime(2028, 2, 29, 12, 0)
+        self.assertEqual(_add_interval(start, Price.Interval.YEAR, 1), datetime(2029, 2, 28, 12, 0))
