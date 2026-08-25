92a7e3f2f5e48d69fef20c07d49a516177768bd8
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-25 12:55:29 +0545

    fix: schedule one-time subscription expiry task

--- config/settings/local.py
+++ config/settings/local.py
@@ -19,7 +19,6 @@ ALLOWED_HOSTS = ["localhost", ".localhost", "0.0.0.0", "127.0.0.1"]  # noqa: S10
 
 # CACHES
 # ------------------------------------------------------------------------------
-# https://docs.djangoproject.com/en/dev/ref/settings/#caches
 CACHES = {
     "default": {
         "BACKEND": "django.core.cache.backends.locmem.LocMemCache",
@@ -33,33 +32,24 @@ EMAIL_BACKEND = env.str(
     "DJANGO_EMAIL_BACKEND",
     default="django.core.mail.backends.console.EmailBackend",
 )
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-host
 EMAIL_HOST = env("EMAIL_HOST")
-# https://docs.djangoproject.com/en/dev/ref/settings/#email-port
 EMAIL_PORT = 1025
 
 # WhiteNoise
 # ------------------------------------------------------------------------------
-# http://whitenoise.evans.io/en/latest/django.html#using-whitenoise-in-development
 INSTALLED_APPS = ["whitenoise.runserver_nostatic", *INSTALLED_APPS]
 
 # django-debug-toolbar
 # ------------------------------------------------------------------------------
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#prerequisites
 INSTALLED_APPS += ["debug_toolbar"]
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#middleware
 MIDDLEWARE += ["debug_toolbar.middleware.DebugToolbarMiddleware"]
-# https://django-debug-toolbar.readthedocs.io/en/latest/configuration.html#debug-toolbar-config
 DEBUG_TOOLBAR_CONFIG = {
     "DISABLE_PANELS": [
         "debug_toolbar.panels.redirects.RedirectsPanel",
-        # Disable profiling panel due to an issue with Python 3.12+:
-        # https://github.com/jazzband/django-debug-toolbar/issues/1875
         "debug_toolbar.panels.profiling.ProfilingPanel",
     ],
     "SHOW_TEMPLATE_CONTEXT": True,
 }
-# https://django-debug-toolbar.readthedocs.io/en/latest/installation.html#internal-ips
 INTERNAL_IPS = ["127.0.0.1", "10.0.2.2"]
 if env("USE_DOCKER") == "yes":
     import socket
@@ -69,7 +59,6 @@ if env("USE_DOCKER") == "yes":
 
 # django-extensions
 # ------------------------------------------------------------------------------
-# https://django-extensions.readthedocs.io/en/latest/installation_instructions.html#configuration
 INSTALLED_APPS += ["django_extensions"]
 
 # Billing
@@ -119,9 +108,6 @@ DJ_REDIS_PANEL_SETTINGS = {
     },
 }
 # Celery
-# ------------------------------------------------------------------------------
-
-# https://docs.celeryq.dev/en/stable/userguide/configuration.html#task-eager-propagates
 CELERY_TASK_EAGER_PROPAGATES = True
 
 # AWS / Floci
@@ -141,7 +127,6 @@ AWS_S3_OBJECT_PARAMETERS = {
     "CacheControl": "max-age=604800, s-maxage=604800, must-revalidate",
 }
 
-# Adding S3-compatible local storage through Floci for development.
 STORAGES = {
     "default": {
         "BACKEND": "storages.backends.s3.S3Storage",
@@ -164,8 +149,8 @@ CELERY_BEAT_SCHEDULE = {
         "task": "django_template.billing.tasks.retry_unprocessed_stripe_webhooks",
         "schedule": 30.0,
     },
-    "expire-billing-entitlements": {
-        "task": "django_template.billing.tasks.expire_entitlements",
+    "expire-one-time-purchases": {
+        "task": "django_template.billing.tasks.expire_one_time_purchases",
         "schedule": 60.0,
     },
 }
