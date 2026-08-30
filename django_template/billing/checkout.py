b0ce610622949b45e5bed0ff949d91dd502dc1cf
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-30 07:31:55 +0545

    remove obsolete billing checkout module

--- django_template/billing/checkout.py
+++
@@ -1,8 +0,0 @@
-"""Backward-compatible checkout imports.
-
-Stripe checkout implementation lives in ``billing.services.stripe``.
-"""
-
-from .services import create_checkout_session, create_or_get_customer
-
-__all__ = ["create_checkout_session", "create_or_get_customer"]
