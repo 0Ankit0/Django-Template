92284ab07243f2eac286c9bfae052f52c6aa075f
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-30 07:32:34 +0545

    remove redundant billing webhook service facade

--- django_template/billing/services/webhooks.py
+++
@@ -1,5 +0,0 @@
-from .stripe_webhooks import STRIPE_WEBHOOK_EVENTS
-from .stripe_webhooks import process_stripe_webhook_event
-from .webhook import handle_webhook
-
-__all__ = ["STRIPE_WEBHOOK_EVENTS", "handle_webhook", "process_stripe_webhook_event"]
