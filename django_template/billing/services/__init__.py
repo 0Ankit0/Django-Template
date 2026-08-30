1a948f24df1f478b74e46a3e8ab44d62c47c4be4
Ankit Poudyal <ankitpaudel.1373@gmail.com>
2026-08-30 07:36:05 +0545

    route one-time subscription lifecycle through payment service

--- django_template/billing/services/__init__.py
+++ django_template/billing/services/__init__.py
@@ -1,17 +1,63 @@
 from .invoice import build_invoice_pdf
-from .providers import GatewayResult, create_esewa_checkout, create_khalti_checkout, esewa_status, khalti_lookup, verify_esewa_response
-from .stripe import _add_interval, _dt, _price_from_event, _stripe_client, _stripe_dict, _stripe_request
-from .stripe import cancel_subscription, change_subscription, create_checkout_session, create_or_get_customer, create_or_update_one_time_subscription
-from .stripe import create_portal_session, create_stripe_customer, create_stripe_price, create_stripe_product, expire_one_time_subscriptions
-from .stripe import get_current_subscription, has_feature, sync_invoice, sync_payment_intent, sync_subscription
+from .payment import add_price_interval, create_or_update_one_time_subscription
+from .providers import (
+    GatewayResult,
+    create_esewa_checkout,
+    create_khalti_checkout,
+    esewa_status,
+    khalti_lookup,
+    verify_esewa_response,
+)
+from .stripe import (
+    _add_interval,
+    _dt,
+    _price_from_event,
+    _stripe_client,
+    _stripe_dict,
+    _stripe_request,
+    cancel_subscription,
+    change_subscription,
+    create_checkout_session,
+    create_or_get_customer,
+    create_portal_session,
+    create_stripe_customer,
+    create_stripe_price,
+    create_stripe_product,
+    expire_one_time_subscriptions,
+    get_current_subscription,
+    has_feature,
+    sync_invoice,
+    sync_payment_intent,
+    sync_subscription,
+)
 from .stripe_webhooks import STRIPE_WEBHOOK_EVENTS, process_stripe_webhook_event
 from .webhook import handle_webhook
 
 __all__ = [
-    "GatewayResult", "STRIPE_WEBHOOK_EVENTS", "build_invoice_pdf", "cancel_subscription", "change_subscription",
-    "create_checkout_session", "create_esewa_checkout", "create_khalti_checkout", "create_or_get_customer",
-    "create_or_update_one_time_subscription", "create_portal_session", "create_stripe_customer", "create_stripe_price",
-    "create_stripe_product", "esewa_status", "expire_one_time_subscriptions", "get_current_subscription",
-    "handle_webhook", "has_feature", "khalti_lookup", "process_stripe_webhook_event", "sync_invoice",
-    "sync_payment_intent", "sync_subscription", "verify_esewa_response",
+    "GatewayResult",
+    "STRIPE_WEBHOOK_EVENTS",
+    "add_price_interval",
+    "build_invoice_pdf",
+    "cancel_subscription",
+    "change_subscription",
+    "create_checkout_session",
+    "create_esewa_checkout",
+    "create_khalti_checkout",
+    "create_or_get_customer",
+    "create_or_update_one_time_subscription",
+    "create_portal_session",
+    "create_stripe_customer",
+    "create_stripe_price",
+    "create_stripe_product",
+    "esewa_status",
+    "expire_one_time_subscriptions",
+    "get_current_subscription",
+    "handle_webhook",
+    "has_feature",
+    "khalti_lookup",
+    "process_stripe_webhook_event",
+    "sync_invoice",
+    "sync_payment_intent",
+    "sync_subscription",
+    "verify_esewa_response",
 ]
